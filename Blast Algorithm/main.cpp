edf99f0e8ff49b7131b2298512854811f83da83d
ahmedhassan135 <45296458+ahmedhassan135@users.noreply.github.com>
2019-12-13 22:31:43 +0500

    Update main.cpp

--- Blast Algorithm/main.cpp
+++ Blast Algorithm/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 #include <cstring>
 
 using namespace std;
 
 int main()
 {
+
+    cout<<"Starting Part 1...\n";
+
     string line;
     string sname;
     string seq;
@@ -20,14 +24,12 @@ int main()
     {
         while ( getline (myfile,line) )
         {
-            //cout << line << '\n';
             if (line[0] == '>')
             {
                 if (!firstSeq)
                 {
                     for (int i=0; i < (seq.length()-k+1); i++)
                     {
-                        //cout<<"."<<seq[i]<<"and"<<seq[i+k-1]<<seq[i+k]<<" for "<<seq<<endl;
                         string kmer = "";
                         for (int j=i; j <= i+k-1; j++)
                         {
@@ -48,7 +50,6 @@ int main()
             {
                 seq = seq + line;
             }
-            //cout << sname << '\n';
         }
     myfile.close();
     }
@@ -65,6 +66,9 @@ int main()
     }
 
     myfile2.close();
+
+
+    cout<<"Finished Part 1...\n";
 
 
     cout<<"\n\n\nExiting...\n";
