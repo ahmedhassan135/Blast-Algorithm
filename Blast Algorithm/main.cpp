3d0007cdb6bc6d5c5e2df0969f0532acbda69c2c
ahmedhassan135 <45296458+ahmedhassan135@users.noreply.github.com>
2019-12-14 00:37:30 +0500

    Update main.cpp

--- Blast Algorithm/main.cpp
+++ Blast Algorithm/main.cpp
@@ -5,6 +5,65 @@
 
 using namespace std;
 
+	class LinkList
+	{
+	private:
+		struct node
+		{
+			string seq;
+			string name;
+			int start;
+			int ending;
+			node *next;
+		};
+
+
+	public:
+		node *head = NULL;
+		node *tail = NULL;
+
+		node *insert_node(string Seq, string Name, int Start, int Ending)
+		{
+			node *newNode = new node;
+
+			newNode->seq = Seq;
+			newNode->name = Name;
+			newNode->start = Start;
+			newNode->ending = Ending;
+			newNode->next = NULL;
+
+
+			if (head == NULL)
+			{
+				head = newNode;
+				tail = newNode;
+				return newNode;
+			}
+
+
+			tail->next = newNode;
+			tail = newNode;
+			return newNode;
+		}
+
+
+
+		void print_all_records()
+		{
+		    cout<<"\nprinting all records\n\n";
+			node *tempptr = head;
+			while (tempptr)
+			{
+				cout << tempptr->seq << ", " << tempptr->name <<", "<<tempptr->start<<", "<<tempptr->ending<<endl;
+				tempptr = tempptr->next;
+			}
+		}
+
+
+	};
+
+
+
 int main()
 {
 
@@ -35,7 +94,7 @@ int main()
                         {
                             kmer = kmer + seq[j];
                         }
-                        myfile2 << kmer <<" "<< sname << endl;
+                        myfile2 << kmer <<" "<< sname <<" "<<i<<" "<<i+k<< endl;
                     }
 
                 }
@@ -62,13 +121,63 @@ int main()
         {
             kmer = kmer + seq[j];
         }
-        myfile2 << kmer <<" "<< sname << endl;
+         myfile2 << kmer <<" "<< sname <<" "<<i<<" "<<i+k<< endl;
     }
 
     myfile2.close();
 
 
     cout<<"Finished Part 1...\n";
+
+    seq = "";
+    sname = "";
+    firstSeq = true;
+
+
+    LinkList querylist;
+
+    ifstream queryfile ("query.txt");
+    ifstream dbfile ("db.txt");
+
+
+    if (queryfile.is_open())
+    {
+        while ( getline (queryfile,line) )
+        {
+            if (line[0] == '>')
+            {
+                if (!firstSeq)
+                {
+                    for (int i=0; i < (seq.length()-k+1); i++)
+                    {
+                        string kmer = "";
+                        for (int j=i; j <= i+k-1; j++)
+                        {
+                            kmer = kmer + seq[j];
+                        }
+                        querylist.insert_node(kmer, sname, i, i+k);
+                        //dbfile << kmer <<" "<< sname <<" "<<i<<" "<<i+k<< endl;
+                    }
+
+                }
+
+                else
+                    firstSeq = false;
+
+                sname = line.substr(1, line.length());
+                seq = "";
+            }
+            else
+            {
+                seq = seq + line;
+            }
+        }
+    myfile.close();
+    }
+    else cout << "Unable to open file";
+
+
+    querylist.print_all_records();
 
 
     cout<<"\n\n\nExiting...\n";
